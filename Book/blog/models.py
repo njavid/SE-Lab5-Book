12ace7ce9bba50b6d3aa76462275ad6377cca515
narges <jatages@gmail.com>
2021-04-22 13:13:19 +0430

    Create Book database

--- Book/blog/models.py
+++ Book/blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.IntegerField(choices=((1,1),(2,2),(3,3),(4,4)))
