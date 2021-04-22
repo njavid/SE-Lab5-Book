fc30d15b6e788b6646cb10350d6ecff006863023
narges <jatages@gmail.com>
2021-04-22 15:33:09 +0430

    Implement create func for book crud

--- Book/blog/models.py
+++ Book/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    category = models.IntegerField(choices=((1,1),(2,2),(3,3),(4,4)))
+    category = models.IntegerField(choices=((1,1),(2,2),(3,3),(4,4)),validators=[MaxValueValidator(4), MinValueValidator(1)])
--- Book/blog/views.py
+++ Book/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from datetime import datetime , timedelta
+from .models import Book
+
+
+
+
+@csrf_exempt
+def create(request):
+    if request.method == 'POST':
+        received_json_data = json.loads(request.body)
+        print(received_json_data["title"])
+        book = Book(
+            title = received_json_data["title"],
+            category = int(received_json_data["category"])
+        )
+        book.save()
+
+        print(request)
+
+        return HttpResponse("created successfully, book id = "+str(book.id),status= 201)
+    else:
+        return HttpResponse(status=401,content="method not allowed!")
